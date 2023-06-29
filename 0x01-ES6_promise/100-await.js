996524509300e0b4a58934ab8eb344e85abb8731
Lerato-Lee <mthimkhululerato72@gmail.com>
2023-06-29 09:22:54 +0000

    correction

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,14 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  let photo;
-  let user;
   try {
-    photo = await uploadPhoto();
-    user = await createUser();
-  } catch (error) {
-    photo = null;
-    user = null;
+    return { photo: await uploadPhoto(), user: await createUser() };
+  } catch (e) {
+    return { photo: null, user: null };
   }
-  return { photo, user };
 }
